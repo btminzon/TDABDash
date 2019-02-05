16cb88dd5092250746f44c7d88d0bb5700c5a1ce
Breno Minzon <breno.minzon@infovista.com>
2019-02-05 15:47:17 +0100

    Add a protection for timestamp in case Dash gets the log before is being formed completely

--- app.py
+++ app.py
@@ -19,19 +19,20 @@ def open_file(filename):
 
 def parse_log(lines, field):
     for line in lines:
-        data["timestamp"].append(line.split(" ")[1].split(".")[0])
-        if field is "temperature" and "temperature:" in line:
-            temperature = parse_line(line, "temperature:")
-            data["temperature"].append(temperature[:2] + '.' + temperature[2:])
-        elif field is "current" and "current_now" in line:
-            current = parse_line(line, "current_now:")
-            data["current"].append(current.replace("\n", ""))
-        elif field is "voltage" and "voltage" in line:
-            voltage = parse_line(line, "voltage:")
-            data["voltage"].append(voltage[:1] + '.' + voltage[1:])
-        elif field is "level" and "level:" in line:
-            level = parse_line(line, "level:")
-            data["level"].append(level)
+        if len(line) > 15:
+            data["timestamp"].append(line.split(" ")[1].split(".")[0])
+            if field is "temperature" and "temperature:" in line:
+                temperature = parse_line(line, "temperature:")
+                data["temperature"].append(temperature[:2] + '.' + temperature[2:])
+            elif field is "current" and "current_now" in line:
+                current = parse_line(line, "current_now:")
+                data["current"].append(current.replace("\n", ""))
+            elif field is "voltage" and "voltage" in line:
+                voltage = parse_line(line, "voltage:")
+                data["voltage"].append(voltage[:1] + '.' + voltage[1:])
+            elif field is "level" and "level:" in line:
+                level = parse_line(line, "level:")
+                data["level"].append(level)
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
