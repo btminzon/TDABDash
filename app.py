b04804562da222aac5d0f139bb7d242ce9b863df
Breno Minzon <breno.minzon@gmail.com>
2019-02-05 10:31:52 +0100

    Display Battery Temperature and Battery Voltage

--- app.py
+++ app.py
@@ -11,12 +11,14 @@ def open_file(filename = "log.txt"):
 
 
 def parse_log(lines):
-    data = {"timestamp": [], "temperature": []}
+    data = {"timestamp": [], "temperature": [], "voltage": []}
     for line in lines:
         if len(line.replace("  ", " ").split(" ")) > 15:
             data["timestamp"].append(line.split(" ")[1].split(".")[0])
-            y = line.replace("  ", " ").split(" ")[14].replace(",", "")
-            data["temperature"].append(y[:2] + '.' + y[2:])
+            temperature = line.replace("  ", " ").split(" ")[14].replace(",", "")
+            data["temperature"].append(temperature[:2] + '.' + temperature[2:])
+            voltage = line.replace("  ", " ").split(" ")[12].replace(",", "")
+            data["voltage"].append(voltage[:1] + '.' + voltage[1:])
     return data
 
 
@@ -25,22 +27,26 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Battery Temperature'),
+    html.H1(children='TEMS Sense DashMonitor'),
 
     html.Div(children='''
         Logs retrieved from IMX using remote ADB. Built using Dash.
-    '''),
+    ''',),
 
-    dcc.Graph(
-        id='batt-graph'
-    ),
+    html.Div([
+        dcc.Graph(id='batt-graph'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
+    html.Div([
+        dcc.Graph(id='volt-graph'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
     dcc.Interval(
         id='interval-component',
-        interval=5 * 1000,  # in milliseconds
+        interval=1 * 1000,  # in milliseconds
         n_intervals=0
     )
 
 ])
+
 
 @app.callback(Output('batt-graph', 'figure'),
               [Input('interval-component', 'n_intervals')])
@@ -55,6 +61,25 @@ def update_batt_graph(n):
         ],
         'layout': {
             'title': 'Battery Temperature',
+            'yaxis': {'type': 'linear'},
+            'xaxis': {'showgrid': False}
+        }
+    }
+
+
+@app.callback(Output('volt-graph', 'figure'),
+              [Input('interval-component', 'n_intervals')])
+def update_volt_graph(n):
+    lines = open_file()
+    data = parse_log(lines)
+    return {
+        'data': [
+            {'x': data["timestamp"],
+             'y': data["voltage"],
+             'type': 'line', 'name': 'V'},
+        ],
+        'layout': {
+            'title': 'Battery Voltage',
             'yaxis': {'type': 'linear'},
             'xaxis': {'showgrid': False}
         }
--- logfetcher.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-server = "temsremote-8b71"
-username = "root"
-password = "!1nf0Vi5ta!"
-ssh = paramiko.SSHClient()
-ssh.load_system_host_keys()
-ssh.connect(server, username=username, password=password)
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(["cd /temp", "adb logcat |grep temperature"])
