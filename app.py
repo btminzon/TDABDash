85a76b241cdb6e14e5ec1d97b24a7728317391be
Breno Minzon <breno.minzon@gmail.com>
2019-02-05 15:03:37 +0100

    Add Battery Current graph measurement

--- app.py
+++ app.py
@@ -5,21 +5,26 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 
-def open_file(filename = "log.txt"):
+data = {"timestamp": [], "temperature": [], "voltage": [], "current": []}
+flag = 0
+
+
+def open_file(filename):
     file = open(filename, "r")
     return file.readlines()
 
 
-def parse_log(lines):
-    data = {"timestamp": [], "temperature": [], "voltage": []}
+def parse_log(lines, field):
     for line in lines:
-        if len(line.replace("  ", " ").split(" ")) > 15:
-            data["timestamp"].append(line.split(" ")[1].split(".")[0])
+        data["timestamp"].append(line.split(" ")[1].split(".")[0])
+        if field is "temperature" and "temperature:" in line:
             temperature = line.replace("  ", " ").split(" ")[14].replace(",", "")
             data["temperature"].append(temperature[:2] + '.' + temperature[2:])
+        elif field is "current" and "current_now" in line:
+            data["current"].append(line.replace("  ", " ").split(" ")[-1].split(":")[1].replace("\n", ""))
+        elif field is "voltage" and "voltage" in line:
             voltage = line.replace("  ", " ").split(" ")[12].replace(",", "")
             data["voltage"].append(voltage[:1] + '.' + voltage[1:])
-    return data
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -39,9 +44,12 @@ app.layout = html.Div(children=[
     html.Div([
         dcc.Graph(id='volt-graph'),
     ], style={'display': 'inline-block', 'width': '49%'}),
+    html.Div([
+        dcc.Graph(id='amp-graph'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
     dcc.Interval(
         id='interval-component',
-        interval=1 * 1000,  # in milliseconds
+        interval=5 * 1000,  # in milliseconds
         n_intervals=0
     )
 
@@ -51,8 +59,10 @@ app.layout = html.Div(children=[
 @app.callback(Output('batt-graph', 'figure'),
               [Input('interval-component', 'n_intervals')])
 def update_batt_graph(n):
-    lines = open_file()
-    data = parse_log(lines)
+    data["temperature"] = []
+    data["timestamp"] = []
+    lines = open_file("log.txt")
+    parse_log(lines, "temperature")
     return {
         'data': [
             {'x': data["timestamp"],
@@ -60,7 +70,7 @@ def update_batt_graph(n):
              'type': 'line', 'name': 'Temp'},
         ],
         'layout': {
-            'title': 'Battery Temperature',
+            'title': 'Battery Temperature (ºC)',
             'yaxis': {'type': 'linear'},
             'xaxis': {'showgrid': True}
         }
@@ -70,8 +80,10 @@ def update_batt_graph(n):
 @app.callback(Output('volt-graph', 'figure'),
               [Input('interval-component', 'n_intervals')])
 def update_volt_graph(n):
-    lines = open_file()
-    data = parse_log(lines)
+    data["voltage"] = []
+    data["timestamp"] = []
+    lines = open_file("log.txt")
+    parse_log(lines, "voltage")
     return {
         'data': [
             {'x': data["timestamp"],
@@ -79,11 +91,33 @@ def update_volt_graph(n):
              'type': 'line', 'name': 'V'},
         ],
         'layout': {
-            'title': 'Battery Voltage',
+            'title': 'Battery Voltage (V)',
             'yaxis': {'type': 'linear'},
             'xaxis': {'showgrid': True}
         }
     }
+
+
+@app.callback(Output('amp-graph', 'figure'),
+              [Input('interval-component', 'n_intervals')])
+def update_amp_graph(n):
+    data["current"] = []
+    data["timestamp"] = []
+    lines = open_file("logA.txt")
+    parse_log(lines, "current")
+    return {
+        'data': [
+            {'x': data["timestamp"],
+             'y': data["current"],
+             'type': 'line', 'name': 'V'},
+        ],
+        'layout': {
+            'title': 'Battery Current (mA)',
+            'yaxis': {'type': 'linear'},
+            'xaxis': {'showgrid': True}
+        }
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
